e641fea885e4f0a6f7780dd5f0522e3b71d69207
Admin <volckove23@gmail.com>
2024-05-24 15:43:48 +0200

    Homework2.2

---
+++ Homework2.2.py
@@ -0,0 +1,34 @@
+n1 = int(input("Введите число: "))
+
+number5 = n1 % 10
+n2 = n1 // 10
+
+number4 = n2 % 10
+n3 = n2 // 10
+
+number3 = n3 % 10
+n4 = n3 // 10
+
+number2 = n4 % 10
+n5 = n4 // 10
+
+number1 = n5 % 10
+n6 = n5 // 10
+
+print(number5 * 10000 + number4 * 1000 + number3 * 100 + number2 * 10 + number1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
