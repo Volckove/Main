c42467e9f66ebcf5fa0eed7177d3fc090d237d88
Volckove <volckove23@gmail.com>
2024-05-24 15:51:52 +0200

    Update ДЗ 2.1.py

--- ДЗ 2.1.py
+++ ДЗ 2.1.py
@@ -1,23 +1,5 @@
-while True:
- user_input = input('Ввести 4-х значне число:')
- number = int(user_input)
- if len(user_input) == 4:
-    print('Введено правильно')
-    break
- else:
-  print('Номер 4-х значний не вказан. Спробуйте ще раз')
 
-left, right = divmod(number, 1000)
-print(left)
-
-left, right = divmod(right, 100)
-print(left)
-
-left, right = divmod(right, 10)
-print(left)
-
-left, right = divmod(right, 1)
-print(left)
+ 
 
 
 
