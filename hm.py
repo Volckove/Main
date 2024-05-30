33cd0ce812c260ede7476e269c00b5afba8aefac
Admin <volckove23@gmail.com>
2024-05-30 03:07:17 +0200

    Homework3.3

---
+++ hm.py
@@ -0,0 +1,18 @@
+First_list = [1,2,3,4,5,6]
+Half1 = First_list[0:3]
+Half2 = First_list[3:6]
+Half3= [Half1,Half2]
+print(First_list)
+print(Half3)
+
+Second_list = [1,2,3]
+Part1 = Second_list[0:2]
+Part2 = Second_list[2:]
+Part3 = [Part1,Part2]
+print(Second_list)
+print(Part3)
+
+Empty_list = []
+print(Empty_list)
+Empty3 = [Empty_list,Empty_list]
+print(Empty3)
