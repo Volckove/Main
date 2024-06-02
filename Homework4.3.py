8c8688304d69a532934702dc4f9903c1bdf0c941
Admin <volckove23@gmail.com>
2024-06-02 02:24:56 +0200

    Homework4.3.py

--- Homework4.3.py
+++ Homework4.3.py
@@ -1,10 +1,12 @@
-first_list = [1,2,3,4,5,6,7,9]
 
-a = first_list
-if (len(a) > 2) and (len(a) < 11):
-  x = a[0]
-  y = a[2]
-  z = a[-2]
+import random
+list = [random.randint(1, 9) for i in range(random.randint(3, 10))]
+print(list)
 
-  second_list = [x,y,z]
-  print(second_list)
+x = list[0]
+y = list[2]
+z = list[-2]
+
+second_list = [x,y,z]
+print(second_list)
+
