64cdd117bcd05190f15ccc310dff3b220a4fd583
Admin <volckove23@gmail.com>
2024-06-06 02:04:41 +0200

    homework5.3.py

---
+++ homework5.3.py
@@ -0,0 +1,18 @@
+import string
+
+text = input('Enter any text:')
+
+for check in string.punctuation:
+    text = text.replace(check,'')
+    text = text.replace(' ','')
+
+first_letter = text[0]
+c = first_letter.upper()
+n = text[1:]
+
+b = '#' + c+n
+
+if len(b) > 140:
+ print(b[:140])
+else:
+ print(b)
