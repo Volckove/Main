bb9e62a50b901744088d2aa462c3e0a7ef1386ba
Admin <volckove23@gmail.com>
2024-06-08 00:02:42 +0200

    homework5.1.py

---
+++ homework5.1.py
@@ -0,0 +1,26 @@
+import keyword
+
+name = input("Enter:")
+
+def variable(name):
+
+    if '0' <= name[0] <= '9':
+        return False
+
+    for find in name:
+        if not (find.islower() or '0' <= find <= '9' or find == '_'):
+            return False
+
+
+    if name in keyword.kwlist:
+        return False
+
+
+    if name.count('_') > 1:
+        return False
+
+    return True
+
+print(variable(name))
+
+
